926d99ad1f8c7f4aeebf4a075dfa5ac0d215fee2
MunisaAbdullayeva <abdullayevam385@gmail.com>
2024-06-26 15:13:34 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,34 @@
+// function decoration
+
+// function qoshish (a, b) {
+//     console.log(a + b)
+// }
+// qoshish(10 ,5)
+
+// function ayirish (c, d) {
+//     console.log(c - d)
+// }
+// ayirish( 10, 5)
+
+// function kopaytirish (e, f) {
+//     console.log(e * f)
+// }
+// kopaytirish( 10, 5)
+
+// function bolish (g, h) {
+//     console.log(g / h)
+// }
+// bolish( 10, 5)
+
+function car () {
+    alert('moshina ochild')
+}
+function car1 () {
+    alert('moshina yopild')
+}
+function car2 () {
+    alert('bagaj ochild')
+}
+function car3 () {
+    alert('moshina zavad qlnd')
+}
